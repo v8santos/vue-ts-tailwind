57f0c6ed0033b3afd84b556da50736a08c687380
Victor Santos <victor.engsantos@gmail.com>
2022-05-18 03:29:55 -0300

    starting new vue-ts(vite) project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import ButtonVue from './components/Button.vue';
+
+const count = ref(0);
+
+const updateCounter = () => {
+  count.value++
+}
+</script>
+
+<template>
+  <ButtonVue :text="count" class="bg-[#5a5fda]" @click="updateCounter"/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  text: string | number,
+  color?: string
+}>()
+
+
+</script>
+
+<template>
+  <button class="rounded-sm px-2 leading-7 focus:ring-1 focus:ring-[#e1a5d2] outline-none">
+    {{ text }}
+  </button>
+</template>
